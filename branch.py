22ee58d04460287824da9a88e97c9543c2a7518d
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2025-09-18 23:13:23 +0530

    m2

--- branch.py
+++ branch.py
@@ -8,10 +8,22 @@ to check branch
 
 to switch branch 
 - git switch branchname
+- git switch -C branchname => create branch first and then switch into that branch
 
 to merge file we need to use branch "main" compolsary
 - git merge branchname => the branch how push that branchname => git merge feature/navbar
 
+merging techniques => fast forward merge, three way merge, squash merging, recursive, strategry merge, rebase and merge
+
+conflicts handle karna
+branch delete karna  => git branch -d branchname
+~ stashing samajhna => git stash => not adding data no commit, but in some where of memory git storing this data, you come next time to this branch you can add before or use it before. 
+
+to retrive this data again cmd 
+- git stash apply
+
+to delete all stash 
+- git stash clear
 
 
 """
