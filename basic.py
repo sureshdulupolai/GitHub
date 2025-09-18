86ecadbe3f9236ef17434c111f485559a589c1df
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2025-09-18 16:34:49 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,37 @@
+"""
+
+Check On Cmd Git is Intall or Not 
+- git
+
+Extension
+- git blame
+- git autoconfig
+optional - git theme
+
+
+start
+-----------------------------------------------------------------------------------------------------
+
+git config --global user.name "name"
+- git should need to now your name, global user name setup
+
+git config --global user.email "email@gmail.com"
+- git set global email
+
+git config --global core.editor "code --wait"
+- "code --wait" => code means vs code mai chalu karna and wait matlab jab tak vs code mai chalu nhi hota tab tak wait likh ke aayega
+
+git config --global core.autocrlf "input"
+- map se data push hoga, varna windows se puch hoga toh central mai jake kaise data save hoga
+
+git config --global -e
+- to check and edit the global value and see the global value
+
+
+------------------------------------------------------------------------------------------------------
+
+git init -> to init project
+git add . -> to add a file, but not pushed "A"
+
+
+"""
