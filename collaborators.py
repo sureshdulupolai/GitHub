76eaf7394565b235984313a625a52072a4f7ee81
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2025-09-19 00:48:31 +0530

    adding more file

---
+++ collaborators.py
@@ -0,0 +1,40 @@
+"""
+Common Steps 
+---------------------------------------------------------------------------------
+- main person need to do
+main banda folder and initial files banaayega
+ab usse github par daal de
+collaborators add kare
+
+saare bande us repo se cloning karein
+[VERY IMPORTANT] apni branch create karein
+apna code usi branch mein likhein
+complete hone par commit dein and push kar dein
+inform karein teammate ko about the commit
+merge banda fetch karega and merge karega and re-push
+
+suresh = Main
+pritam = collab
+
+suresh 
+- create a project
+- push in github
+- add collab to pritam
+git init, git add ., git commit -m "", git remote, git push
+
+pritam
+- need to clone 
+- create branch
+- only add ., git commit -m "", git push -u origin branchname
+
+
+suresh
+- can check switch with pritam or merge
+- need to merge => git merge barnchname
+- need to push in github to central branch => git push origin main
+
+pritam
+- need fetch same code => git fetch => git pull
+
+
+"""
